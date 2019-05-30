0369fae8e87f65873bdc24efdea1c0aa9c12b102
SunGod1998 <batvind98@gmail.com>
2019-05-30 11:19:39 +0530

    changed

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,18 +1,28 @@
 echo "Welcome to the guessing game!"
-#to get the number of files : ls -l | egrep -c "^-"
+value=$(ls -l | egrep -c "^-")
+get_low(){
+  low = $(expr $1 - 2)
+  return $low
+}
+get_up(){
+  up = $(expr $1 + 2)
+  return $up
+}
+up= $(get_up $value)
+low= $(get_low $value)
 let flag=0
 while [[ $flag == 0 ]]
 do
   echo "Enter your guess of the number of files present in the current directory :"
   read guess
-  if [[ $guess -eq 3 ]]
+  if [[ $guess -eq $value ]]
   then
     echo "Congratulations! You guessed it right!"
     let flag=1
-  elif [[ $guess -gt 4 ]]
+  elif [[ $guess -gt $up ]]
   then
     echo "Too high!.....Try again!"
-  elif [[ $guess -lt 2 ]]
+  elif [[ $guess -lt $low ]]
   then
     echo "Too low!.....Try again!"
   else
